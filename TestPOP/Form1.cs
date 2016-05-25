ee4edf2005ca57197f9a9bd189e0311f98de7c26
aleksandr-st-novikov <aleksandr.st.novikov@gmail.com>
2016-05-25 21:11:33 +0300

    Добавил Mail.dll

--- TestPOP/Form1.cs
+++ TestPOP/Form1.cs
@@ -19,7 +19,20 @@ namespace TestPOP
 
         private void simpleButton1_Click(object sender, EventArgs e)
         {
+            List<string> seenUids = new List<string>();
+            seenUids.Add("531d2cb32c09c3a8c5c6c36ba1b88cae");
+            List<OpenPop.Mime.Message> mes = FetchUnseenMessages("pop.yandex.ru", 995, true, "test@e-tiande.by", "test0101", seenUids);
 
+            foreach (OpenPop.Mime.Message msg in mes)
+            {
+                var att = msg.FindAllAttachments();
+                foreach (var ado in att)
+                {
+                    ado.Save(new System.IO.FileInfo(System.IO.Path.Combine("d:\\att", ado.FileName)));
+                }
+            }
+
+            string ff = "bfaac90cc38811c94cf67738372c87e6";
         }
 
         public static List<OpenPop.Mime.Message> FetchUnseenMessages(
@@ -71,6 +84,47 @@ namespace TestPOP
                 // Return our new found messages
                 return newMessages;
             }
+        }
+
+
+        public static void DeleteMessageOnServer(string hostname, int port, bool useSsl, string username, string password, string Uid)
+        {
+            // The client disconnects from the server when being disposed
+            using (Pop3Client client = new Pop3Client())
+            {
+                // Connect to the server
+                client.Connect(hostname, port, useSsl);
+
+                // Authenticate ourselves towards the server
+                client.Authenticate(username, password);
+
+                // Mark the message as deleted
+                // Notice that it is only MARKED as deleted
+                // POP3 requires you to "commit" the changes
+                // which is done by sending a QUIT command to the server
+                // You can also reset all marked messages, by sending a RSET command.
+
+                int messageCount = client.GetMessageCount();
+
+                // Run trough each of these messages and download the headers
+                for (int messageItem = messageCount; messageItem > 0; messageItem--)
+                {
+                    var ddd = client.GetMessageHeaders(messageItem);
+                    var dfd = client.GetMessageUid(messageItem);
+                    // If the Message ID of the current message is the same as the parameter given, delete that message
+                    if (client.GetMessageUid(messageItem) == Uid)
+                    //if (client.GetMessageHeaders(messageItem).MessageId == Uid)
+                    {
+                        // Delete
+                        client.DeleteMessage(messageItem);
+                    }
+                }
+            }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            DeleteMessageOnServer("pop.yandex.ru", 995, true, "test@e-tiande.by", "test0101", "bfaac90cc38811c94cf67738372c87e6");
         }
     }
 }
