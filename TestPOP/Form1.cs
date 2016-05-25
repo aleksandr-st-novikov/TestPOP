e00a10fc051fe7494afc6d354547e87b1aa5d513
aleksandr-st-novikov <aleksandr.st.novikov@gmail.com>
2016-05-25 19:18:42 +0300

    1

--- TestPOP/Form1.cs
+++ TestPOP/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenPop.Pop3;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,62 @@ namespace TestPOP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public static List<OpenPop.Mime.Message> FetchUnseenMessages(
+            string hostname, 
+            int port, 
+            bool useSsl, 
+            string username, 
+            string password, 
+            List<string> seenUids)
+        {
+            // The client disconnects from the server when being disposed
+            using (Pop3Client client = new Pop3Client())
+            {
+                // Connect to the server
+                client.Connect(hostname, port, useSsl);
+
+                // Authenticate ourselves towards the server
+                client.Authenticate(username, password);
+
+                // Fetch all the current uids seen
+                List<string> uids = client.GetMessageUids();
+
+                // Create a list we can return with all new messages
+                List<OpenPop.Mime.Message> newMessages = new List<OpenPop.Mime.Message>();
+
+                // All the new messages not seen by the POP3 client
+                for (int i = 0; i < uids.Count; i++)
+                {
+                    string currentUidOnServer = uids[i];
+                    if (!seenUids.Contains(currentUidOnServer))
+                    {
+                        // We have not seen this message before.
+                        // Download it and add this new uid to seen uids
+
+                        // the uids list is in messageNumber order - meaning that the first
+                        // uid in the list has messageNumber of 1, and the second has 
+                        // messageNumber 2. Therefore we can fetch the message using
+                        // i + 1 since messageNumber should be in range [1, messageCount]
+                        OpenPop.Mime.Message unseenMessage = client.GetMessage(i + 1);
+
+                        // Add the message to the new messages
+                        newMessages.Add(unseenMessage);
+
+                        // Add the uid to the seen uids, as it has now been seen
+                        seenUids.Add(currentUidOnServer);
+                    }
+                }
+
+                // Return our new found messages
+                return newMessages;
+            }
         }
     }
 }
